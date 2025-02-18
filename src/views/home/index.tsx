ca8e7164c8632a502dce31fb7576d0ce3f6f28c8
Nicolas Balmaceda <nbalmaceda@koronet.com>
2025-02-17 22:29:39 -0300

    changes to main landing page

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -31,14 +31,14 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
-        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
-        <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
-          Solana Next
+        <div className='text-sm font-normal align-bottom text-right text-slate-900 mt-4'>v{pkg.version}</div>
+        <h1 className="text-center text-5xl md:pl-12 font-bold bg-clip-text text-black mb-4">
+          Peer Wallet
         </h1>
         </div>
         <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
-          <p>Unleash the full power of blockchain with Solana and Next.js 13.</p>
-          <p className='text-slate-500 text-2x1 leading-relaxed'>Full-stack Solana applications made easy.</p>
+          <p className='text-slate-900 font-semibold'>Unleash the power of the seamless, secure, and smart way to manage your digital assets anytime, anywhere.</p><br></br>
+          <p className='text-slate-700 text-2x1 leading-relaxed'>Secure & Seamless Transactions made easy.</p>
         </h4>
         <div className="relative group">
           <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-indigo-500 rounded-lg blur opacity-40 animate-tilt"></div>
