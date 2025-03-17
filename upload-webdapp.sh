d71376412c0a7ed936f9a9d7a8408ddfc25f829f
Nicolas Balmaceda <balmacedanicolas4@gmail.com>
2025-03-17 20:04:47 -0300

    update webdapp upload sh

--- upload-webdapp.sh
+++ upload-webdapp.sh
@@ -6,4 +6,5 @@ aws s3 sync ./out s3://splforge-webapp/
 aws s3 mv s3://splforge-webapp/tokencreator.html s3://splforge-webapp/tokencreator
 aws s3 mv s3://splforge-webapp/dashboard.html s3://splforge-webapp/dashboard
 aws s3 mv s3://splforge-webapp/liquiditypool.html s3://splforge-webapp/liquiditypool
+aws s3 mv s3://splforge-webapp/about.html s3://splforge-webapp/about
 aws cloudfront create-invalidation --distribution-id E3GMELXQKVHDTB --paths "/*"
